106e64838f857551fb23525dc66532b469cfe8dc
18310243633t <tl18310243633@163.com>
2022-04-28 16:14:26 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd ./public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:tl-study/tl-study.github.io.git main:gh-pages
+git push -f https://tl-study/tl-study.github.io.git main:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
